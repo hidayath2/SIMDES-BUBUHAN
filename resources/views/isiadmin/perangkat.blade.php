12857c5f933ea2e6b21c73b965555bf1d2427e32
hidayath2 <hidayat27042002@gmail.com>
2022-09-15 00:36:00 +0700

    perbaiki eror detail perangkat

--- resources/views/isiadmin/perangkat.blade.php
+++ resources/views/isiadmin/perangkat.blade.php
@@ -12,7 +12,7 @@
                 <div class="d-flex flex-column align-items-center text-center">
             
                   {{-- <img src="{{ asset('storage/gambar-perangkat/' . $daftarperangkat->image) }}" alt="Admin" class="rounded-circle" width="150">    --}}
-                  <img src="{{ asset('storage/gambar-perangkat/' . $daftarperangkat->image) }}" alt="Admin" class="rounded-circle" width="150">             
+                  <img src="{{ asset('storage/public/gambar-perangkat/' . $daftarperangkat->image) }}" alt="Admin" class="rounded-circle" width="150">             
 
                   {{-- <div class="mt-3">
                     <h4>{{ $daftarmasyarakat->nama }}</h4>
