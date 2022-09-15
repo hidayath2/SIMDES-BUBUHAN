46269ecf939f3b30e88f8deab6e196aa2d8e9561
hidayath2 <hidayat27042002@gmail.com>
2022-09-15 14:43:43 +0700

    perbaiki data peribadi

--- resources/views/masyarakatdashboard/sidebar.blade.php
+++ resources/views/masyarakatdashboard/sidebar.blade.php
@@ -2,9 +2,9 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center">
+    {{-- <a class="sidebar-brand d-flex align-items-center justify-content-center">
         <div class="sidebar-brand-text mx-3">Masyarakat</sup></div>
-    </a>
+    </a> --}}
 
     <li class="nav-item">
         <a class="nav-link" href="/datapribadi">
